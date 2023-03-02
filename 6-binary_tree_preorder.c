91a203ed6fb4205fdb22ad27d86132c19550b139
Esther-06 <wairimuw@gmail.com>
2023-03-02 03:53:38 -0800

    binary trees

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,10 +5,11 @@
  *@tree: pointer to the root node
  *@func: pointer to a function
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)){
-	if((!tree) || (!func))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if ((!tree) || (!func))
 		return;
 	func(tree->n);
-	binary_tree_preorder(tree->left,func);
-	binary_tree_preorder(tree->right,func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }

dacc3aab67ed6adde08a99bfcebc640bebe5d9aa
Esther-06 <wairimuw@gmail.com>
2023-03-02 04:06:22 -0800

    binary trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,19 +12,19 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node_right;
+	binary_tree_t *new_right;
 
 	if (parent == NULL)
 		return (NULL);
 
-	node_right = binary_tree_node(parent, value);
-	if (node_right == NULL)
+	new_right = binary_tree_node(parent, value);
+	if (new_right == NULL)
 		return (NULL);
 	if (parent->right)
 	{
-		parent->right->parent = node_right;
-		node_right->right = parent->right;
+		parent->right->parent = new_right;
+		new_right->right = parent->right;
 	}
-	parent->right = node_right;
+	parent->right = new_right;
 	return (node_right);
 }

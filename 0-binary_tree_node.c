841eab2f0285ec5edf1604c369679b4a69fa4ba8
Esther-06 <wairimuw@gmail.com>
2023-03-02 03:36:01 -0800

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,12 +10,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof((binary_tree_t));
-	if (new == NULL)
-	return (NULL);
-	new -> n = value;
-	new -> parent = parent;
-	new -> left = NULL;
-	new -> right = NULL;
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+	new->parent = parent;
 	return (new);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,7 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,7 +20,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
@@ -28,5 +27,14 @@ typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
